a17a5b7b56894d195690970ddf14a1c30c7a27c1
Lucas Bispo <lucasbispocorrea@gmail.com>
2026-07-31 16:05:48 -0300

    Envio dos Arquivos do projeto para o repositório Remoto

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let deNumero = document.getElementById('de').value;
+    let ateNumero = document.getElementById('ate').value;
+
+    alert(`Sorteando ${quantidade} números entre ${deNumero} e ${ateNumero}...`);
+}
